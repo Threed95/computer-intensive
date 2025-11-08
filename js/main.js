fe4f409896f6002f73567ac4a397fe33b12fab27
Threed95 <bolshanovad95@gmail.com>
2025-11-08 14:07:06 +0300

    добавила js для модального окна

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const modalWindow = document.querySelector('.modal');
+
+const buttonModals = document.querySelectorAll('.button__modal');
+
+const modalClose = document.querySelector('.modal__close');
+
+const body = document.querySelector('body');
+
+buttonModals.forEach((item) => {
+    item.addEventListener('click', () => {
+        modalWindow.style.display = 'flex';
+        body.classList.add('noscroll');
+    });
+
+})
+
+modalWindow.addEventListener('click', (e) => {
+    const isModal = e.target.closest('.modal__inner');
+
+    if (!isModal) {
+        modalWindow.style.display = 'none';
+        body.classList.remove('noscroll');
+    }
+
+})
+
+modalClose.addEventListener('click', () => {
+    modalWindow.style.display = 'none';
+    body.classList.remove('noscroll');
+})
